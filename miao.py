14494771a22919e1b8d72e0961f5941f95b27eb5
lxyddice <95132858+lxyddice@users.noreply.github.com>
2023-07-02 21:34:42 +0800

    智齿符号

--- miao.py
+++ miao.py
@@ -4,7 +4,13 @@ import time
 
 while True:
     text = input("请输入文本: ")
-    text += "喵"
+    if text.endswith('！') or text.endswith('~') or text.endswith('？'):
+        suffix = text[-1]
+        text = text[:-1]
+    else:
+        suffix = ''
+    text += "喵" + suffix
+    
     keyboard = Controller()
     pyautogui.hotkey('alt', 'tab')
     time.sleep(0.1)
